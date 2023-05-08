e286036e1b924cc5703ecbf9f4d43e0e90cf89dd
Felipe Lima <102830741+LipePLima@users.noreply.github.com>
2023-05-08 19:07:41 -0300

    desenvolvendo a alteração do saldo

--- src/components/balance/index.tsx
+++ src/components/balance/index.tsx
@@ -1,11 +1,29 @@
+import { FiCheck } from 'react-icons/fi'
+import { AiOutlineEdit } from 'react-icons/ai'
 import "./Balance.scss"
 
 const Balance = () => {
+     const changeBalance = (e: Element) => {
+          const balance = e.parentNode?.querySelector('.balance__balance') as Element;
+
+     }
+
      return (
-          <section className="body__balance">
+          <section className="container__balance">
                <div className='balance__info'>
-                    <h1>Meus saldo</h1>
-                    <p>R$ 499,00</p>
+                    <div>
+                         <h1>Meus saldo</h1>
+                         <div className='balance__balance'>
+                              <p>R$ 499,00</p>
+                              <input type="number" />
+                         </div>
+                    </div>
+                    <div onClick={ e => changeBalance(e.currentTarget)}>
+                         <AiOutlineEdit
+                              className="balance__icon"
+                              size={26}
+                         />
+                    </div>
                </div>
                <div className='balance__circles'>
                     <div className='circles__circleBorder'></div>
